d1e8920819287430f237e533bb55fff6324ebbb5
30526 <taiburrahamanapu@gmail.com>
2025-11-04 22:36:27 +0600

    make the card section responsive

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -3,8 +3,8 @@ import { GiCheckMark } from "react-icons/gi";
 
 const PricingCard = () => {
   return (
-    <div className="flex justify-center gap-4 pb-30 py-10 bg-blue-50">
-      <div className="w-sm pb-15 bg-white h-fit translate-y-8">
+    <div className="flex flex-col md:flex-row justify-center items-center gap-4 pb-30 py-10 bg-blue-50">
+      <div className="w-sm pb-15 bg-white h-fit translate-y-8 mb-15">
         <div className="py-6">
           <h2 className="text-2xl font-bold my-5 text-center">
            Single learning program
